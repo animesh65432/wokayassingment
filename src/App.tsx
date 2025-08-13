771e6416b4441ed1e03165c26faea385c07913e3
Your Name <your.email@example.com>
2025-08-13 08:33:04 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='flex h-dvh w-full justify-center items-center'>
