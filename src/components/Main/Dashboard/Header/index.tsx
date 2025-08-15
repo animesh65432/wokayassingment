e4ca84b54f0507036d6c8a8195b0379a2b27da9c
Your Name <your.email@example.com>
2025-08-15 13:43:31 +0530

    update

--- src/components/Main/Dashboard/Header/index.tsx
+++ src/components/Main/Dashboard/Header/index.tsx
@@ -1,18 +1,18 @@
 import { SidebarTrigger } from "@/components/ui/sidebar"
-import { UsersRound, Slash, Kanban, Ellipsis, HatGlasses, Share2 } from "lucide-react"
+import { Plus, ChartNoAxesGantt, Calendar, UsersRound, Slash, Kanban, Ellipsis, HatGlasses, Share2, CalendarSync, BotMessageSquare, List, Table, Search, Settings, ListChecks } from "lucide-react"
 
 type Props = {
     IsSidebarOpen: boolean
 }
 export default function Header({ IsSidebarOpen }: Props) {
     return (
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full ">
             <div className="flex items-center justify-between px-2 h-14 w-full border border-[rgb(53,52,52)]">
                 <div className="flex gap-4 items-center shrink">
                     {!IsSidebarOpen && <SidebarTrigger className="text-white" />}
                     <div className="flex h-4 items-center gap-1">
                         <div className="bg-[rgba(62,99,221,1)] p-1 rounded">
-                            <UsersRound className="h-3 w-3 text-white" />
+                            <UsersRound className="h-3 w-3" />
                         </div>
                         <p className="text-sm ">Team Space</p>
                         <div>
@@ -20,8 +20,8 @@ export default function Header({ IsSidebarOpen }: Props) {
                         </div>
                     </div>
                     <div className="flex items-center gap-1">
-                        <Kanban className="h-4 w-4 text-white" />
-                        <p className="text-white text-sm">Project 1</p>
+                        <Kanban className="h-4 w-4" />
+                        <p className="text-sm">Project 1</p>
                     </div>
                     <div>
                         <Ellipsis className="h-3 w-3" />
@@ -33,11 +33,11 @@ export default function Header({ IsSidebarOpen }: Props) {
                         <p className="text-sm hidden lg:block">agents</p>
                     </div>
                     <div className="flex gap-1 items-center border border-[rgb(255_255_255/15%)] p-1 rounded">
-                        <HatGlasses className="h-4 w-4" />
+                        <CalendarSync className="h-4 w-4" />
                         <p className="text-sm hidden lg:block">automate</p>
                     </div>
                     <div className="flex gap-1 items-center border border-[rgb(255_255_255/15%)] p-1 rounded">
-                        <HatGlasses className="h-4 w-4" />
+                        <BotMessageSquare className="h-4 w-4" />
                         <p className="text-sm hidden lg:block">Ask Ai</p>
                     </div>
                     <div className="flex gap-1 items-center border border-[rgb(255_255_255/15%)] p-1 rounded">
@@ -46,7 +46,53 @@ export default function Header({ IsSidebarOpen }: Props) {
                     </div>
                 </div>
             </div>
-            <div></div>
+            <div className="flex text-[rgb(255_258_248/50%)] justify-between px-2 items-center h-14 border-b  border-[rgb(53,52,52)]">
+                <div className="flex gap-4">
+                    <div className="flex items-center gap-1">
+                        <List className="h-4 w-4" />
+                        <p className="text-sm">List</p>
+                    </div>
+                    <div className="flex lg:hidden items-center">
+                        <p className="text-sm">4 More...</p>
+                    </div>
+
+                    <div className=" hidden lg:flex items-center gap-1">
+                        <Kanban className="h-4 w-4" />
+                        <p className="text-sm">Board</p>
+                    </div>
+                    <div className="hidden lg:flex items-center gap-1">
+                        <Calendar className="h-4 w-4" />
+                        <p className="text-sm">Calendar</p>
+                    </div>
+                    <div className=" hidden lg:flex items-center gap-1">
+                        <ChartNoAxesGantt className="h-4 w-4" />
+                        <p className="text-sm">Gantt</p>
+                    </div>
+                    <div className="hidden lg:flex items-center gap-1">
+                        <Table className="h-4 w-4" />
+                        <p className="text-sm">Gantt</p>
+                    </div>
+                    <div className="border-l border-[rgb(53,52,52)]"></div>
+                    <div className="flex items-center gap-1">
+                        <Plus className="h-4 w-4" />
+                        <p className="text-sm  hidden md:block">View</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-1">
+                        <Search className="h-4 w-4" />
+                        <p className="text-sm  hidden md:block">Search</p>
+                    </div>
+                    <div className=" md:flex items-center gap-1">
+                        <ListChecks className="h-4 w-4" />
+                        <p className="text-sm hidden md:block">Hide</p>
+                    </div>
+                    <div className="flex items-center gap-1">
+                        <Settings className="h-4 w-4" />
+                        <p className="text-sm  hidden md:block text-[rgb(255_258_248/50%)]">Customize</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
